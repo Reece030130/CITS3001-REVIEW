806c85b454d9d6a837f051e03c9140589754bccb
Reece030130 <167398987+Reece030130@users.noreply.github.com>
2024-10-26 00:34:38 +0800

    right gostonepuzzle.py http://domjudge.gozz.au/team/submission/14268

--- lab4/gostonepuzzle.py
+++ lab4/gostonepuzzle.py
@@ -1,33 +1,36 @@
 from collections import deque
 
-
 def gostonepuzzle(s, t):
-    if sorted(s) != sorted(t):
-        return -1
     queue = deque([(s, 0)])
-    visited = {s}
+    visited = set(s)
     while queue:
         current_state, moves = queue.popleft()
-        moves += 1
-        dot_index = current_state.find('..')
+        if current_state == t:
+            return moves
+        empty_positions = []
+        for i in range(len(current_state)):
+            if current_state[i] == '.':
+                empty_positions.append(i)
         for i in range(len(current_state) - 1):
-            if i == dot_index or i == dot_index - 1:
-                continue
-            temp = list(current_state)
-            temp[dot_index], temp[dot_index + 1] = temp[i], temp[i + 1]
-            temp[i], temp[i + 1] = '.', '.'
-            new_state = ''.join(temp)
-            if new_state[:-2] == t[:-2]:
-                return moves
-            if new_state not in visited:
-                queue.append((new_state, moves))
-                visited.add(new_state)
+            if current_state[i] != '.' and current_state[i + 1] != '.':
+                new_state = current_state.copy()
+                stones = [new_state[i], new_state[i + 1]]
+                new_state[i] = '.'
+                new_state[i + 1] = '.'
+                new_state[empty_positions[0]] = stones[0]
+                new_state[empty_positions[1]] = stones[1]
+                if new_state == t:
+                    return moves + 1
+                state_tuple = tuple(new_state)
+                if state_tuple not in visited:
+                    visited.add(state_tuple)
+                    queue.append((new_state, moves + 1))
 
     return -1
 
 
 if __name__ == '__main__':
     length = int(input().strip())
-    initial_stone = input().strip() + ".."
-    target_stone = input().strip() + ".."
+    initial_stone = list(input().strip())+['.'] * 2
+    target_stone = list(input().strip())+['.'] * 2
     print(gostonepuzzle(initial_stone, target_stone))
