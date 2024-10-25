edf5ae1810e5381107b0e5fe25fe9ab1e1883d62
Reece030130 <167398987+Reece030130@users.noreply.github.com>
2024-10-25 12:07:01 +0800

    Final solution correct http://domjudge.gozz.au/team/submission/14243

--- mountainwaterfall.py
+++ mountainwaterfall.py
@@ -1,38 +1,29 @@
 def mountainwaterfall(mountain: list, row: int, column: int):
-    flowed = []
     stack_pos = []
-    def flowing():
-        while stack_pos:
-            water_row, water_col = stack_pos.pop()
-            if (water_row+1, water_col+0) not in flowed and mountain[water_row+1][water_col+0] != 'o':
-                flowed.append((water_row+1, water_col+0))
-                stack_pos.append((water_row+1, water_col+0))
-                mountain[water_row+1][water_col+0] = '*'
-                break
-            else:
-                if ((water_row+0, water_col+1) not in flowed and (water_row+0, water_col-1) not in flowed and mountain[water_row][water_col+1] != 'o' or
-                        mountain[water_row][water_col-1] != 'o'):
-                    flowed.append((water_row+0, water_col+1))
-                    stack_pos.append((water_row+0, water_col+1))
-                    mountain[water_row][water_col+1] = '*'
-                    flowed.append((water_row+0, water_col-1))
-                    stack_pos.append((water_row+0, water_col-1))
-                    mountain[water_row][water_col-1] = '*'
-                break
     for i in range(row):
         for j in range(column):
-            if mountain[i][j] == '*' and mountain[i][j] not in flowed:
+            if mountain[i][j] == '*':
                 stack_pos.append((i,j))
-                flowing()
+                while stack_pos:
+                    water_row, water_col = stack_pos.pop()
+                    if mountain[water_row+1][water_col] != '*':
+                        if mountain[water_row + 1][
+                            water_col + 0] == '.' and 0 <= water_col+1 <= column and 0 <= water_row+0 <= row:
+                            stack_pos.append((water_row + 1, water_col + 0))
+                            mountain[water_row + 1][water_col + 0] = '*'
+                        else:
+                            for r_move, c_move in [(0, -1), (0, 1)]:
+                                new_row = water_row + r_move
+                                new_col = water_col + c_move
+                                if 0 <= new_col < column and mountain[new_row][new_col] == '.':
+                                    stack_pos.append((new_row, new_col))
+                                    mountain[new_row][new_col] = '*'
 
     return mountain
 
 if __name__ == '__main__':
     row, column = map(int, input().split())
-    len = row
-    mountain = []
-    while len:
-        mountain.append(list(input()))
-        len -= 1
-    for i in mountainwaterfall(mountain, row, column):
+    mountain = [list(input()) for _ in range(row)]
+    result = mountainwaterfall(mountain, row, column)
+    for i in result:
         print(*i, sep='')
